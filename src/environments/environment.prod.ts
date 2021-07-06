6d4c46619d586f32e63e9407578d14a55538cc6f
Tinh Le <ltrongtinh.dev@gmail.com>
2021-07-06 14:03:43 +0700

    Git fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBpIOS3f66VUT5obTFPS7kx19AoECVd3JA",
+    authDomain: "webtuthien.firebaseapp.com",
+    databaseURL: "https://webtuthien-default-rtdb.firebaseio.com",
+    projectId: "webtuthien",
+    storageBucket: "webtuthien.appspot.com",
+    messagingSenderId: "102889078940",
+    appId: "1:102889078940:web:9781a11557fac530db6d59",
+    measurementId: "G-KFPKJERCVC"
+  }
 };
