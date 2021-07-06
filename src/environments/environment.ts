61430f364caca1849580f986277d6109ea857eb3
Tinh Le <tinhle@Tinhs-MacBook-Air.local>
2021-07-06 12:51:03 +0700

    Fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyBpIOS3f66VUT5obTFPS7kx19AoECVd3JA",
     authDomain: "webtuthien.firebaseapp.com",
